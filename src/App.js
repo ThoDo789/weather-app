fb7cd6c1ddbaface52d5888d14cf6a4aa261a0e0
tho do <thodo7199@gmail.com>
2021-04-15 22:46:55 +0700

    get api weather

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, { useEffect, useState } from 'react';
+import CurrentLocation from './components/current-location';
+import ShowWeather from './components/Show-weather';
+import PlacesAutocomplete from './components/weather';
+
+const App = () => {
+  const[currentLocation,setCurrentLocation]=useState(null);
+  const[latLng,setLatLog] = useState(null);
+  const [weather,setWeather] =useState(null);
+ 
+  const handleLocation=(data)=>{
+    // setCurrentLocation(data)
+  }
+ 
+  const handleLatLng=(latLng)=>{
+   setLatLog(latLng)
+  }
+  console.log(currentLocation, latLng)
+  useEffect(()=>{
+   async function fetchWeather(){
+      try{
+       
+        const url = `http://api.openweathermap.org/data/2.5/weather?lat=${latLng.lat}&lon=${latLng.lng}&appid=e1a5f0e6dbe87d1067ada6dd9a2ee751`;
+        const res = await fetch(url);
+        const response = await res.json();
+       setWeather(response)
+    
+        
+      }catch(error){
+        
+      }
+    }
+    fetchWeather()
+
+  },[latLng])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <CurrentLocation getCurrent={handleLocation}/>
+      <PlacesAutocomplete getLatLngLocation={handleLatLng}/>
+      <ShowWeather weather={weather}/>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/current-location/index.js
@@ -0,0 +1,29 @@
+
+import React from 'react';
+import { useCurrentPosition } from 'react-use-geolocation';
+
+ 
+function CurrentLocation(props) {
+  const [position, error] = useCurrentPosition();
+const{getCurrent} = props
+  if (!position && !error) {
+    return <p>Waiting...</p>;
+  }
+ 
+  if (error) {
+    return <p>{error.message}</p>;
+  }
+  getCurrent(position)
+  return (
+    <div>
+      <p>
+        Latitude: {position.coords.latitude}
+      </p>
+      <p>
+        Longitude: {position.coords.longitude}
+      </p>
+
+    </div>
+  );
+}
+export default CurrentLocation
