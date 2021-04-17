e993734caca38616d53afdd11d88b77d54ac3d4f
tho do <thodo7199@gmail.com>
2021-04-17 12:14:10 +0700

    update css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
+import { useCurrentPosition, useWatchPosition } from 'react-use-geolocation';
 import "./App.css";
 import BgWeather from "./components/bgWeather";
 import DetailsWeather from "./components/DetailWeather";
-import CurrentLocation from "./components/DetailWeather/current-location";
+
 import Clear from "./images/Atmosphere.jpg";
 import Atmosphere from "./images/Clear.jpg";
 import Clouds from "./images/Clouds.jpg";
@@ -11,21 +12,34 @@ import Rain from "./images/Rain.jpg";
 import Snow from "./images/Snow.jpg";
 import Thunderstorm from "./images/Thunderstorm.jpg";
 const App = () => {
-  const [currentLocation, setCurrentLocation] = useState(null);
-  const [latLng, setLatLog] = useState(null);
+  
+ 
   const [weather, setWeather] = useState(null);
- const [bg,setBg] = useState('')
-  const handleLocation = data => {
-    // setCurrentLocation(data)
-  };
-
+  const [position,error] = useWatchPosition()
+  const [latLng, setLatLog] = useState({lat:0,lng:0});
+ 
   const callHandleLatLng = latLng => {
     setLatLog(latLng);
   };
-  console.log(currentLocation, latLng);
+  console.log(latLng);
+  // useEffect(()=>{
+  //   setLatLog({
+  //    lat:position.coords.latitude,
+  //    lng:position.coords.longitude
+
+  //   })
+  // },[])
   useEffect(() => {
     async function fetchWeather() {
       try {
+      //   console.log(latLng.lat)
+      //   if(position.coords!==null){
+      //  setLatLog({
+      //     lat:position.coords.latitude,
+      //     lng:position.coords.longitude
+      //   })
+      //     console.log(latLng.lat)
+      //   }
         const url = `http://api.openweathermap.org/data/2.5/weather?lat=${latLng.lat}&lon=${latLng.lng}&appid=e1a5f0e6dbe87d1067ada6dd9a2ee751`;
         const res = await fetch(url);
         const response = await res.json();
@@ -34,7 +48,7 @@ const App = () => {
       } catch (error) {}
     }
     fetchWeather();
-  }, [latLng]);
+  }, [latLng,position]);
 
   
   if(weather!==null){
@@ -59,11 +73,11 @@ const App = () => {
         return description;
   
       } else if (code > 700 &&
-         code < 800) {
+         code < 750) {
         description = Atmosphere;
         return description;
   
-      } else if (code === 800) {
+      } else if (code===800) {
         description = Clear;
         return description;
   
@@ -81,20 +95,29 @@ const App = () => {
    var bgImg = desc()
    console.log(bgImg)
   }
+  
 
+  // if (!position && !error) {
+  //   return <p>Waiting...</p>;
+  // }
+ 
+  // if (error) {
+  //   return <p>{error.message}</p>;
+  // }
+ 
 
   return (
     <div className="app" style={{backgroundImage:`url(${bgImg})`}}>
       <div className="app-weather">
        
         <div className="container wrapper-col">
-        <CurrentLocation getCurrent={handleLocation} />
+        {/* <CurrentLocation /> */}
           <div className="row wrapper-col">
             
-            <div className="col-8 wrapper-col bg" style={{backgroundImage:`url(${bgImg})`}}>
+            <div className="col-lg-8 col-sm-12 wrapper-col bg" style={{backgroundImage:`url(${bgImg})`}}>
               <BgWeather propsWeather={weather} />
               </div>
-              <div className="col-4 details">
+              <div className="col-lg-4 col-sm-12 details">
               <DetailsWeather
                 handleLatLng={callHandleLatLng}
                 propsWeather={weather}
--- src/components/DetailWeather/current-location/index.js
+++ src/components/DetailWeather/current-location/index.js
@@ -1,29 +1,29 @@
 
-import React from 'react';
-import { useCurrentPosition } from 'react-use-geolocation';
+// import React from 'react';
+// import { useCurrentPosition } from 'react-use-geolocation';
 
  
-function CurrentLocation(props) {
-  const [position, error] = useCurrentPosition();
-const{getCurrent} = props
-  if (!position && !error) {
-    return <p>Waiting...</p>;
-  }
+// function CurrentLocation(props) {
+//   const [position, error] = useCurrentPosition();
+// const{getCurrent} = props
+//   if (!position && !error) {
+//     return <p>Waiting...</p>;
+//   }
  
-  if (error) {
-    return <p>{error.message}</p>;
-  }
-  getCurrent(position)
-  return (
-    <div>
-      {/* <p>
-        Latitude: {position.coords.latitude}
-      </p>
-      <p>
-        Longitude: {position.coords.longitude}
-      </p> */}
+//   if (error) {
+//     return <p>{error.message}</p>;
+//   }
+//   getCurrent(position)
+//   return (
+//     <div>
+//       {/* <p>
+//         Latitude: {position.coords.latitude}
+//       </p>
+//       <p>
+//         Longitude: {position.coords.longitude}
+//       </p> */}
 
-    </div>
-  );
-}
-export default CurrentLocation
+//     </div>
+//   );
+// }
+// export default CurrentLocation
